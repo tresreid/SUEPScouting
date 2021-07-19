38ac3d0abf9336b51161f1e2b89553f669d3046b
Benedikt Maier <bmaier@submit.mit.edu>
2021-07-19 10:43:00 -0400

    anomaly

--- test/ScoutingNanoAOD_cfg.py
+++ test/ScoutingNanoAOD_cfg.py
@@ -2,120 +2,111 @@ import FWCore.ParameterSet.Config as cms
 import FWCore.Utilities.FileUtils as FileUtils
 import os
 
-
-
-# Set parameters externally 
+# Set parameters externally
 from FWCore.ParameterSet.VarParsing import VarParsing
 params = VarParsing('analysis')
 
 params.register(
-    'isMC', 
-    False, 
+    'isMC',
+    True,
     VarParsing.multiplicity.singleton,VarParsing.varType.bool,
     'Flag to indicate whether the sample is simulation or data'
 )
 
 params.register(
-    'useWeights', 
-    False, 
+    'useWeights',
+    False,
     VarParsing.multiplicity.singleton,VarParsing.varType.bool,
     'Flag to indicate whether or not to use the events weights from a Monte Carlo generator'
 )
 
 params.register(
-    'filterTrigger', 
-    False, 
+    'filterTrigger',
+    False,
     VarParsing.multiplicity.singleton,VarParsing.varType.bool,
     'Flag to indicate whether or not to ask the event to fire a trigger used in the analysis'
 )
 
 params.register(
-    'filterMuons', 
-    False, 
+    'filterMuons',
+    False,
     VarParsing.multiplicity.singleton,VarParsing.varType.bool,
     'Flag to indicate whether or not to ask the event to contain at least two muons'
 )
 
 params.register(
-    'reducedInfo', 
-    False, 
+    'reducedInfo',
+    False,
     VarParsing.multiplicity.singleton,VarParsing.varType.bool,
     'Flag to indicate whether or not to store just the reduced information'
 )
 
 params.register(
-    'trigProcess', 
-    'HLT', 
+    'trigProcess',
+    'HLT',
     VarParsing.multiplicity.singleton,VarParsing.varType.string,
     'Process name for the HLT paths'
 )
 
 params.register(
-    'GlobalTagData', 
-    #'101X_dataRun2_HLT_v7',
-    '101X_dataRun2_Prompt_v11', 
+    'GlobalTagData',
+    '<not available>',
     VarParsing.multiplicity.singleton,VarParsing.varType.string,
     'Process name for the HLT paths'
 )
 
 params.register(
-    'GlobalTagMC', 
-    '102X_upgrade2018_realistic_v15', 
+    'GlobalTagMC',
+    '112X_mcRun3_2021_realistic_v16',
     VarParsing.multiplicity.singleton,VarParsing.varType.string,
     'Process name for the HLT paths'
 )
 
 params.register(
-    'xsec', 
-    0.001, 
+    'xsec',
+    0.001,
     VarParsing.multiplicity.singleton,VarParsing.varType.float,
     'Cross-section for a Monte Carlo Sample'
 )
+
 params.register(
-    'fileList', 
-    'none', 
+    'fileList',
+    'none',
     VarParsing.multiplicity.singleton,VarParsing.varType.string,
     'input list of files'
 )
 
 params.setDefault(
-    'maxEvents', 
-    10 
+    'maxEvents',
+    10
 )
 
 params.setDefault(
-    'outputFile', 
-    'test.root' 
+    'outputFile',
+    'test.root'
 )
-
 
 
 # Define the process
 process = cms.Process("LL")
 
 # Parse command line arguments
-print("Made it here")
 params.parseArguments()
-print("Not here")
 
 # Message Logger settings
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.destinations = ['cout', 'cerr']
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000 
-#process.MessageLogger.cerr.threshold = 'INFO'
-#process.MessageLogger.cerr.INFO = cms.untracked.PSet(
-#    limit = cms.untracked.int32(-1)
-#)
+process.MessageLogger.cerr.FwkReport.reportEvery = 5
 
 # Set the process options -- Display summary at the end, enable unscheduled execution
-process.options = cms.untracked.PSet( 
+process.options = cms.untracked.PSet(
     allowUnscheduled = cms.untracked.bool(True),
     wantSummary      = cms.untracked.bool(True),
     #SkipEvent = cms.untracked.vstring('ProductNotFound')
 )
 
 # How many events to process
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(params.maxEvents) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 # Input EDM files
 #list = FileUtils.loadListFromFile(options.inputFiles)
@@ -127,13 +118,13 @@ else :
     readFiles = cms.untracked.vstring( FileUtils.loadListFromFile (os.environ['CMSSW_BASE']+'/src/PhysicsTools/SUEPScouting/'+params.fileList) )
 print("we shall see")
 process.source = cms.Source("PoolSource",
-	fileNames = cms.untracked.vstring(readFiles) 
+        fileNames = cms.untracked.vstring(readFiles) 
 )
 
 # Load the standard set of configuration modules
 process.load('Configuration.StandardSequences.Services_cff')
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
-process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
 
 ##--- l1 stage2 digis ---
@@ -143,61 +134,53 @@ process.load('PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff')
 
 # Load the global tag
 from Configuration.AlCa.GlobalTag import GlobalTag
-if params.isMC : 
+if params.isMC :
     process.GlobalTag.globaltag = params.GlobalTagMC
 else :
     process.GlobalTag.globaltag = params.GlobalTagData
 
 # Define the services needed for the treemaker
-process.TFileService = cms.Service("TFileService", 
-    fileName = cms.string(params.outputFile)
-    #fileName = cms.string("scoutingData18.root")
-    #fileName = cms.string("scoutingQCD500to700.root")
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string(params.outputFile)                                
 )
 
 # Tree for the generator weights
+'''
 process.gentree = cms.EDAnalyzer("LHEWeightsTreeMaker",
     lheInfo = cms.InputTag("externalLHEProducer"),
     genInfo = cms.InputTag("generator"),
     useLHEWeights = cms.bool(params.useWeights)
 )
+'''
 
-
-HLTInfo = []
-L1Info = []
-
+#from DarkPhotonAnalysis.DimuonAnalysis2018.TriggerPaths_cfi import getL1Conf
+L1Info = ['L1_DoubleMu4p5er2p0_SQ_OS_Mass_Min7', 'L1_DoubleMu_12_5','L1_DoubleMu_15_7','L1_TripleMu_5_3_3','L1_TripleMu_5_5_3','L1_QuadMu0','L1_DoubleMu0er1p5_SQ_OS_dR_Max1p4','L1_DoubleMu4p5er2p0_SQ_OS_Mass7to18','L1_DoubleMu4_SQ_OS_dR_Max1p2','L1_SingleMu22','L1_DoubleMu0er1p4_SQ_OS_dR_Max1p4','L1_DoubleMu4p5_SQ_OS_dR_Max1p2','L1_DoubleMu4p5_SQ_OS','L1_DoubleMu0er1p5_SQ_dR_Max1p4','L1_DoubleMu0er2p0_SQ_dR_Max1p4','L1_DoubleMu0_SQ']
 # Make tree
 process.mmtree = cms.EDAnalyzer('ScoutingNanoAOD',
-	
-    hltProcess=cms.string("HLT"),
-    bits          = cms.InputTag("TriggerResults", "", "HLT"),
-	doL1 = cms.bool(True),
-    
-    #	triggerresults   = cms.InputTag("TriggerResults", "", params.trigProcess),
-    #    triggerConfiguration = cms.PSet(
-    #		hltResults            = cms.InputTag('TriggerResults','','HLT'),
-    #		l1tResults            = cms.InputTag(''),
-    #		daqPartitions         = cms.uint32(1),
-    #		l1tIgnoreMaskAndPrescale = cms.bool(False),
-    #		throw                 = cms.bool(False)
-  	#),
-	#ReadPrescalesFromFile = cms.bool( False ),
-    AlgInputTag = cms.InputTag("gtStage2Digis"),
-    l1tAlgBlkInputTag = cms.InputTag("gtStage2Digis"),
-    l1tExtBlkInputTag = cms.InputTag("gtStage2Digis"),
-    l1Seeds           = cms.vstring(L1Info),
-    hltSeeds          = cms.vstring(HLTInfo),
+
+    	triggerresults   = cms.InputTag("TriggerResults", "", params.trigProcess),
+        doL1 = cms.bool(False),
+        triggerConfiguration = cms.PSet(
+    		hltResults            = cms.InputTag('TriggerResults','','HLT'),
+    		l1tResults            = cms.InputTag(''),
+    		daqPartitions         = cms.uint32(1),
+    		l1tIgnoreMaskAndPrescale = cms.bool(False),
+    		throw                 = cms.bool(False)
+  	),
+	ReadPrescalesFromFile = cms.bool( False ),
+        AlgInputTag       = cms.InputTag("gtStage2Digis"),
+        l1tAlgBlkInputTag = cms.InputTag("gtStage2Digis"),
+        l1tExtBlkInputTag = cms.InputTag("gtStage2Digis"),
+        l1Seeds           = cms.vstring(L1Info),
 	#vertices         = cms.InputTag("hltScoutingMuonPacker","displacedVtx"),
 	muons            = cms.InputTag("hltScoutingMuonPacker"),
 	electrons        = cms.InputTag("hltScoutingEgammaPacker"),
-    photons          = cms.InputTag("hltScoutingEgammaPacker"),
+        photons          = cms.InputTag("hltScoutingEgammaPacker"),
 	pfcands          = cms.InputTag("hltScoutingPFPacker"),
 	pfjets           = cms.InputTag("hltScoutingPFPacker"),
-    vertices         = cms.InputTag("hltScoutingPrimaryVertexPacker","primaryVtx"),
-    #pileupinfo       = cms.InputTag("addPileupInfo"),
-    #geneventinfo     = cms.InputTag("generator"),
+        tracks           = cms.InputTag("hltScoutingTrackPacker"),
+    	#pileupinfo       = cms.InputTag("addPileupInfo"),
+    	#geneventinfo     = cms.InputTag("generator"),
 
 )
-#process.Tracer = cms.Service("Tracer")
-
 process.p = cms.Path(                  process.mmtree)
