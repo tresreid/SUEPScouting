0f0363a6f6c511762dec2b9c836ab724cf4389f5
tresreid <tres.reid@gmail.com>
2022-09-15 08:22:17 -0500

    add rho2 for already included rho

--- test/ScoutingNanoAOD_cfg.py
+++ test/ScoutingNanoAOD_cfg.py
@@ -262,6 +262,7 @@ process.mmtree = cms.EDAnalyzer('ScoutingNanoAOD',
 	#vertices         = cms.InputTag("hltScoutingMuonPacker","displacedVtx"),
     #geneventinfo     = cms.InputTag("generator"),
     rho          = cms.InputTag("fixedGridRhoFastjetAllScouting"),
+    rho2          = cms.InputTag("hltScoutingPFPacker","rho"),
 
     # for JEC corrections eventually
     #L1corrAK4_DATA = cms.FileInPath('CMSDIJET/DijetScoutingRootTreeMaker/data/80X_dataRun2_HLT_v12/80X_dataRun2_HLT_v12_L1FastJet_AK4CaloHLT.txt'),
